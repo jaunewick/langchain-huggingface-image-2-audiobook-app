7282f75073b819c16ca193444a1fe3e8b5b99bea
Daniel Giao <daniel.giao@icloud.com>
2024-08-19 02:20:03 -0400

    Implemented img2text

---
+++ app.py
@@ -0,0 +1,18 @@
+from dotenv import find_dotenv, load_dotenv
+from transformers import pipeline
+
+load_dotenv(find_dotenv())
+
+# img2text
+def img2text(url):
+    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    text = image_to_text(url)[0]['generated_text']
+    print(text)
+    return text
+
+img2text("test.png")
+
+# llm
+
+#text2speech
+
